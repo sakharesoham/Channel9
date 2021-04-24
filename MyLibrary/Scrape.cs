da46a07e2e35e0319879f725e1ac8b2ef906ec5e
Soham Sakhare <67227428+sakharesoham@users.noreply.github.com>
2021-04-24 23:20:40 +0530

    Add files via upload

---
+++ MyLibrary/Scrape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace MyLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebPage(string url)
+        {
+            return GetWebPage(url);
+        }
+
+        public string ScrapeWebPage(string url,string filepath) 
+        {
+            string reply = GetWebPage(url);
+
+            File.WriteAllText(filepath,reply);
+            return reply;
+        }
+
+        private string GetWebPage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
